eb783766f5a644b74d0c9826095affa1ecc8c879
严贤良 <yanxianliang@codyy.com>
2018-07-04 15:20:37 +0800

    add browser autoOpen cli

---
+++ cli/open.js
@@ -0,0 +1,9 @@
+/**
+ * @disc:$DESC$
+ * @type:$TYPE$
+ * @dependence:$DEPENDENCE$
+ * @author:yanxinaliang
+ * @time：2018/7/4 15:18
+ */
+var open = require("open");
+open("http://localhost:8000/examples/eboard-demo.html");
