b41af1f604e4f63724e27b6df223c7ace65af91b
Heng Li <liheeng@gmail.com>
2018-07-05 15:47:45 +0800

    Added comment.

--- src/plugins/IPlugin.ts
+++ src/plugins/IPlugin.ts
@@ -2,8 +2,8 @@
  * @Author: Liheng (liheeng@gmail.com)
  * @Date: 2018-06-01 16:53:37
  * @Last Modified by: Liheng (liheeng@gmail.com)
- * @Last Modified time: 2018-06-01 16:54:59
+ * @Last Modified time: 2018-07-05 15:47:07
  */
-export default interface IPlugin {
-    
-}
+// export default interface IPlugin {
+//     // TODO...
+// }
