d8aabba669d695e1940333fb2cf614c7b74c0efc
zhangtianA <zhangtian@codyy.com>
2018-10-16 18:15:48 +0800

    导航样式

--- src/components/Toolbar.ts
+++ src/components/Toolbar.ts
@@ -18,6 +18,7 @@ export declare interface IToolbarItem{
     icon:string;
     key:string;
     active?:boolean;
+    children?:[];
 }
 
 
@@ -112,6 +113,92 @@ const items:IToolbarItem[][]=[[{
     key:'ferule',
 }]];
 
+const newItems:IToolbarItem[][]=[
+    [{
+        name:'选择',
+        icon:'xuanze',
+        key:'xuanze',
+    }],[{
+        name:'画笔',
+        icon:'huabi',
+        key:'huabi',
+        children: [4,8,12,16],
+    }],[{
+        name:'文字',
+        icon:'wenzi',
+        key:'wenzi',
+        children: [14,18,24,26],
+    }],[{
+        name: '图形',
+        icon: 'tuxing',
+        key: 'tuxing',
+    },{
+        name:'直线',
+        icon:'zhixian',
+        key:'zhixian',
+    },{
+        name:'实心圆',
+        icon:'shixinyuan',
+        key:'shixinyuan',
+    },{
+        name:'实心五角星',
+        icon:'shixinxing',
+        key:'shixinxing',
+    },{
+        name:'实心三角形',
+        icon:'shixinsanjiao',
+        key:'shixinsanjiao',
+    },{
+        name:'实心矩形',
+        icon:'shixinfangxing',
+        key:'shixinfangxing',
+    },{
+        name:'箭头',
+        icon:'jiantou',
+        key:'jiantou',
+    },{
+        name:'空心圆',
+        icon:'kongxinyuan',
+        key:'kongxinyuan',
+    },{
+        name:'空心五角星',
+        icon:'kongxinxing',
+        key:'kongxinxing',
+    },{
+        name:'空心三角形',
+        icon:'kongxinsanjiao',
+        key:'kongxinsanjiao',
+    },{
+        name:'空心矩形',
+        icon:'kongxinfangxing',
+        key:'kongxinfangxing',
+    }],[{
+        name:'清空',
+        icon:'qingkong',
+        key:'qingkong',
+        active:false,
+    }],[{
+        name:'教鞭',
+        icon:'jiaobian',
+        key:'jiaobian',
+    }]
+];
+
+const colors = [
+    "#ffffff",
+    "#888888",
+    "#555555",
+    "#000000",
+    "#f22500",
+    "#f66c00",
+    "#fad500",
+    "#64cb00",
+    "#00cac4",
+    "#3698f3",
+    "#8b6dc5",
+    "#ff7c81"
+    ];
+
 
 class ToolbarChildren{
     private from:HTMLDivElement;
@@ -133,7 +220,44 @@ class ToolbarChildren{
         wrap.style.top = offsetTop+offsetHeight + 5 +'px';
         wrap.style.left = offsetLeft + 'px';
         this.dom=wrap;
+        const colorDom = document.createElement('div');
+        colorDom.className = `eboard-toolbar-category-color`;
+        colors.forEach((item)=>{
+            const itemDom = document.createElement('div');
+            itemDom.className = `eboard-toolbar-category-color-item`;
+            itemDom.style.backgroundColor = item;
+            colorDom.appendChild(itemDom);
+            itemDom.addEventListener('click',()=>{
+
+            });
+        });
+        const lineDom = document.createElement('div');
+        lineDom.className = `eboard-toolbar-category-line`;
+        this.dom.appendChild(lineDom);
+        this.dom.appendChild(colorDom);
         this.from.parentElement&&this.from.parentElement.appendChild(wrap);
+
+
+        if(this.items[0].children){
+            const itemDom = document.createElement('div');
+            if(this.items[0].icon === "huabi"){
+                itemDom.className = "eboard-toolbar-huabi";
+            }else if(this.items[0].icon === "wenzi"){
+                itemDom.className = "eboard-toolbar-wenzi";
+            }
+            this.items[0].children.forEach((item1,index1)=>{
+                const itemDom1 = document.createElement('div');
+                itemDom1.className = `eboard-toolbar-item1`;
+                if(this.items[0].icon === "huabi"){
+                    itemDom1.style.width = item1 + "px";
+                    itemDom1.style.height = item1 + "px";
+                }else if(this.items[0].icon === "wenzi"){
+                    itemDom1.innerText = item1 + "";
+                }
+                itemDom.appendChild(itemDom1);
+            });
+            this.dom.appendChild(itemDom);
+        }
     }
     private initRemoveListener(){
         // 才创建就能收到click事件
@@ -148,7 +272,9 @@ class ToolbarChildren{
             const itemDom = document.createElement('div');
             itemDom.title=item.name;
             itemDom.className = `eboard-toolbar-item eboard-icon eboard-icon-${item.icon}`;
-            this.dom.appendChild(itemDom);
+            if(index){
+                this.dom.appendChild(itemDom);
+            }
             itemDom.addEventListener('click',()=>{
                 this.listener&&this.listener.call(this,item);
                 this.from.setAttribute('active',index.toString());
@@ -173,16 +299,68 @@ class Toolbar{
         this.listener=listener;
         this.eBoard=eBoard;
         this.initWrap();
-        this.initItems();
+        // this.initItems();
+        this.initNewItems();
         container.innerHTML='';
         container.appendChild(this.dom);
-        this.createPicker();
-        this.initFontSizePicker();
+        // this.createPicker();
+        // this.initFontSizePicker();
     }
     private initWrap(){
         const wrap = document.createElement('div');
         wrap.className='eboard-toolbar';
         this.dom=wrap;
+    }
+    private initNewItems(){
+        newItems.forEach((members)=>{
+            const itemDom = document.createElement('div');
+            const length = members.length;
+            const item = members[0];
+            itemDom.title=item.name;
+            itemDom.className = `eboard-toolbar-item eboard-icon eboard-icon-${item.icon} ${length>1||members[0].children?'eboard-toolbar-expend':''}`;
+            itemDom.setAttribute('active','0');
+            if(length>1||members[0].children){
+                itemDom.innerHTML='<i class=\'eboard-icon eboard-icon-expend\'/>';
+            }
+            this.dom.appendChild(itemDom);
+            let timer:any;
+            const startEvent=(event:any)=>{
+                event.cancelBubble = true;
+                event.stopPropagation();
+                this.closeChild();
+                const activeItem = Number(itemDom.getAttribute('active'))||0;
+                const key = members[activeItem].key;
+                const active = members[activeItem].active;
+                if(key !== this.activeKey){
+                    this.listener&&this.listener.call(this,members[activeItem]);// 可能不是该item
+                }
+                if(active !== false){
+                    const active = itemDom.parentElement?itemDom.parentElement.querySelector('.active'):undefined;
+                    active&&active.classList.remove('active');
+                    itemDom.classList.add('active');// 移除
+                    this.activeKey= key;
+                }
+                if(length>1||members[0].children){
+                    timer = setTimeout(()=>{
+                        clearTimeout(timer);
+                        timer = undefined as any;
+                        const newMembers = [...members];
+                        // newMembers.shift();
+                        this.showChild(itemDom,newMembers);
+                    },300);
+                }
+            };
+            itemDom.addEventListener('touchstart',startEvent);
+            itemDom.addEventListener('mousedown',startEvent);
+            itemDom.addEventListener('click',(event:any)=>{
+                event.cancelBubble = true;
+                event.stopPropagation();
+                if(timer){
+                    clearTimeout(timer);
+                    timer = undefined as any;
+                }
+            });
+        });
     }
     private initItems(){
         items.forEach((members)=>{
